222abe36a6c42fd57a1f2b8d35a6e75b26a6ec07
snc29654 <snc29654@nifty.com>
2024-04-25 14:55:57 +0900

    aaa

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import hogehoge from './components/hogehoge.vue'
 <template>
 
   <hoge>
-    <img alt="Vue logo" class="logo" src="./assets/image0.jpg" width="300"  /><br>
+    <img alt="Vue logo" class="logo" src="./image0.jpg" width="300"  /><br>
       <hogehoge msg="これは東京駅" /><br>
   </hoge>
 
